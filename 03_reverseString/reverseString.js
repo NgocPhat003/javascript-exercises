e6008dc1b0fa3ba853f3684b87391aa5ca5435e0
NgocPhat003 <ngocphat2405@gmail.com>
2023-08-13 20:49:26 +0700

    Finishing exercises 1 to 7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(baseString) {
+    let reverseString = baseString.split("").reverse().join("");
+    
+    return reverseString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumb, secondNumb) {
+    if(firstNumb<0 || secondNumb<0 || !Number.isInteger(firstNumb) || !Number.isInteger(secondNumb))
+        return 'ERROR';
+    let smallerNumb = firstNumb<secondNumb ? firstNumb : secondNumb;
+    let largerNumb = firstNumb>secondNumb ? firstNumb : secondNumb;
+    let sum = 0;
+    for(let i=smallerNumb; i<=largerNumb; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
